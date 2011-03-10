cbb075b80bf06df1a05b4e2fe690b8307a7235b4
uqasz <ukasz83@gmail.com>
2011-03-10 09:17:19 +0000

    added parsing from file, added no id submenus support

--- mtcfengine.php
+++ mtcfengine.php
@@ -19,32 +19,32 @@ class ukasz_api extends routeros_api {
 
 $API = new ukasz_api();
 $API->debug = true;
-#$hostname='10.194.3.241';
-$hostname='172.30.30.60';
-$user='api';
+$hostname='10.194.3.249';
+#$hostname='172.30.30.60';
+$user='admin';
 $pass='dupa';
 
+if ($API->connect($hostname, $user, $pass)) {
+$data_from_file = file('configfile', FILE_SKIP_EMPTY_LINES);
+$tobeset = array();
+sort($data_from_file);
+foreach ($data_from_file as $key => $line) {
+$rows = preg_split("/[\s]+/", $line);
+$where = trim(array_shift($rows), "/");
+array_pop($rows);
+foreach ($rows as $to_build) {
+	list ($keyy, $value) = explode("=", $to_build);
+	$tmp[$keyy] = $value;
+}
+$tobeset[$where][] = $tmp;
+unset($tmp);
+}
+$clear_attrs = array(); 
+foreach ($tobeset as $tobesetid => $tobesetarray) {
 
- if ($API->connect($hostname, $user, $pass)) {
-  $array = array( 
-  0 => array(
-    'action' => 'remote',
-    'topics' => 'info,!wireless,!debug,!route,!ospf'), 
-  1 => array(
-    'action' => 'remote',
-    'topics' => 'warning'), 
-  2 => array(
-    'action' => 'remote',
-    'topics' => 'error'), 
-  3 => array(
-    'action' => 'echo',
-    'topics' => 'critical')
-);
-$clear_attrs = array('prefix'); 
-
-  $API->write('/system/logging/print');
+  $API->write('/' . $tobesetid . '/print');
   $has = $API->read(true);
-  foreach($array as $id => $data) {
+  foreach($tobesetarray as $id => $data) {
     $elem = isset($has[$id]) ? $has[$id] : array();
     $diff = array_diff($data,  $elem);
     foreach($clear_attrs as $attr) {
@@ -54,36 +54,41 @@ $clear_attrs = array('prefix');
     }
     if ($diff) {
       if (empty($elem)) {
-        $API->write('/system/logging/add', false);
+        $API->write('/' . $tobesetid . '/add', false);
         $API->write_array($data);
         $API->read();
       } else { 
-        $API->write('/system/logging/set', false);
-        $API->write('=.id=' . $elem['.id'], false);
+        $API->write('/' . $tobesetid . '/set', false);
+		if (array_key_exists('.id', $elem)) {
+        		$API->write('=.id=' . $elem['.id'], false);
+		}
         $API->write_array($diff);
         $API->read();
       }
     }
-	if (array_key_exists('.id', $elem)) {
-		$save[] = $elem['.id'];
-	}
+        if (array_key_exists('.id', $elem)) {
+                $save[] = $elem['.id'];
+        }
   }
   foreach($has as $id => $value) {
-	if (isset($value['.id'])) {
-		$collect_ids[] = $value['.id'];
+        if (isset($value['.id'])) {
+                $collect_ids[] = $value['.id'];
+                }
+        }
+	if (!empty($collect_ids) && !empty($save)) {
+    		$remove = array_diff($collect_ids, $save);
 		}
-	}
-    $remove = array_diff($collect_ids, $save);
-	if (!empty($remove)) {
-		$API->write('/system/logging/remove', false);
-    		$tmp = array_shift($remove);
-    		$command = '=.id=' . $tmp; 
-    		foreach($remove as $id => $value) {
-     			$command .= ',' . $value;
-    		}
-	$API->write($command);
-	$API->read();
-	}	
-$API->disconnect();	    
+        if (!empty($remove)) {
+                $API->write('/' . $tobesetid . '/remove', false);
+                $tmp = array_shift($remove);
+                $command = '=.id=' . $tmp; 
+                foreach($remove as $id => $value) {
+                        $command .= ',' . $value;
+                }
+        $API->write($command);
+        $API->read();
+        }         
+}   
+$API->disconnect();   
 };
 ?>
