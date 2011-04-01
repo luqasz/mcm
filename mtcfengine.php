402a078ba5562235c64e693c51b22803e439d9fa
uqasz <ukasz83@gmail.com>
2011-04-01 14:57:58 +0000

    converted mtcfengine to classes added error, info and debug functions in routeros_api

--- mtcfengine.php
+++ mtcfengine.php
@@ -1,121 +1,234 @@
 <?php
-require('routeros_api.class.php');
+require ('routeros_api.class.php');
 class ukasz_api extends routeros_api {
-
-public function write_array(array $array) {
-    $count = count($array);
-    $is_last = 1 == $count;
-    $i = 1;
-    foreach($array as $name => $value) {
-      $this->write('='.$name.'='.$value, $is_last);
-      $i++;
-     if ($i==$count) {
-       $is_last = true;
-     } 
-    }
-  }
- 
-}
-
-$API = new ukasz_api();
-
-$shortopts 	= "p:u:a:vhl:t:c:";
-
-//check for keys in array
-function array_keys_exist($keys,$array) {
-    if (count (array_intersect($keys,array_keys($array))) == count($keys)) {
-        return true;
-    }
-}
-
-//must specify user password and address
-$mandatoryoptions = array('p','u','a');
-$options = getopt($shortopts);
-if (array_key_exists('h', $options)) {
-	echo "You must specify -u username, -p password and -a address\n";
-	echo "\t-a address. can be fqdn\n";
-	echo "\t-c configuration file for address. defaults to specified address.conf\n";
-	echo "\t-d delay between connection attempts in seconds (defaults to 2)\n";
-	echo "\t-h this help\n";
-	echo "\t-p password\n";
-	echo "\t-t connection attempt timeout and data read timeout (defaults to 2)\n";
-	echo "\t-u username\n";
-	echo "\t-v show debug information. be verbose what is going on\n";
-	} else {
-		if (array_keys_exist($mandatoryoptions,$options)) {
-			$API->debug = array_key_exists('v', $options); 
-			$configfile = array_key_exists('c', $options) ? $options['c'] : $options['a'] . '.conf'; 
-			$API->delay = array_key_exists('d', $options) ? $options['d'] : 2; 
-			$API->timeout = array_key_exists('t', $options) ? $options['t'] : 2; 
-			
-				if ($API->connect($options['a'], $options['u'], $options['p'])) {
-					$data_from_file = file($configfile, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
-					$data_from_file = (preg_grep('/^#.*/', $data_from_file, PREG_GREP_INVERT));
-					$tobeset = array();
-
-					foreach ($data_from_file as $line) {
-						$rows = preg_split("/[\s]+/", $line);
-						$where = trim(array_shift($rows), "/");
-							foreach ($rows as $to_build) {
-								list ($key, $value) = explode("=", $to_build);
-								$tmp[$key] = $value;
-							}
-							$tobeset[$where][] = $tmp;
-							unset($tmp);
-					}				
-					$clear_attrs = array(); 
-					foreach ($tobeset as $tobesetid => $tobesetarray) {
-						$API->write('/' . $tobesetid . '/print');
-						$has = $API->read(true);
-							foreach($tobesetarray as $id => $data) {
-								$elem = isset($has[$id]) ? $has[$id] : array();
-								$diff = array_diff($data,  $elem);
-									foreach($clear_attrs as $attr) {
-										if (array_key_exists($attr, $elem) && $elem[$attr] != '') {
-											$diff[$attr] = '';
-										}
-									}
-								if ($diff) {
-									if (empty($elem)) {
-										$API->write('/' . $tobesetid . '/add', false);
-										$API->write_array($data);
-										$API->read();
-									} else { 
-										$API->write('/' . $tobesetid . '/set', false);
-										if (array_key_exists('.id', $elem)) {
-											$API->write('=.id=' . $elem['.id'], false);
-										}
-										$API->write_array($diff);
-										$API->read();
-									}
-								}
-									if (array_key_exists('.id', $elem)) {
-										$save[] = $elem['.id'];
-									}
-								}
-							foreach($has as $id => $value) {
-								if (isset($value['.id'])) {
-									$collect_ids[] = $value['.id'];
-								}
-							}
-						if (!empty($collect_ids) && !empty($save)) {
-							$remove = array_diff($collect_ids, $save);
-						}
-						if (!empty($remove)) {
-							$API->write('/' . $tobesetid . '/remove', false);
-							$tmp = array_shift($remove);
-							$command = '=.id=' . $tmp; 
-								foreach($remove as $id => $value) {
-									$command .= ',' . $value;
-								}
-							$API->write($command);
-							$API->read();
-						}         
-					}   
-					$API->disconnect();   
-				}
-			} else {
-			echo "You must specify -u username, -p password, -a address\n";
+	public function write_array(array $array) {
+		$count = count ( $array );
+		$is_last = 1 == $count;
+		$i = 1;
+		foreach ( $array as $name => $value ) {
+			$this->write ( '=' . $name . '=' . $value, $is_last );
+			$i ++;
+			if ($i == $count) {
+				$is_last = true;
+			}
 		}
-};
+	}
+}
+
+function show_help() {
+?>
+You must specify -u -p -a
+	-a address. can be fqdn
+	-c configuration file for address. defaults to specified address.conf
+	-d delay between connection attempts in seconds (defaults to 2)
+	-h this help
+	-p password
+	-t connection attempt timeout and data read timeout (defaults to 2)
+	-u username
+	-v show debug information. be verbose what is going on
+<?php
+}
+
+function show_connection_error() {
+?>
+Connection fault
+<?php
+}
+
+function show_file_error($file) {
+echo "Specified file \"$file\" does not exist.\n";
+}
+
+
+class Parser {
+
+	public function __construct($file) {
+		$this->_file	= $file;
+	}
+
+	public function parseFile() {
+     	$data_from_file = file ( $this->_file, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES );
+		$data_from_file = (preg_grep ( '/^#.*/', $data_from_file, PREG_GREP_INVERT ));
+
+		foreach ( $data_from_file as $line ) {
+			$rows = preg_split ( "/[\s]+/", $line );
+			$where = trim ( array_shift ( $rows ), "/" );
+			foreach ( $rows as $to_build ) {
+		  		list ( $key, $value ) = explode ( "=", $to_build );
+					$tmp [$key] = $value;
+				}
+				$tobeset [$where] [] = $tmp;
+				unset ( $tmp );
+		}
+		return array($tobeset);
+    }
+}
+
+class Mtcfengine {
+
+    public function __construct($address, $user, $password) {
+		$this->_address  = $address;
+		$this->_user     = $user;
+		$this->_password = $password;
+		$this->api       = new ukasz_api();
+    }
+
+	public function connect() {
+	return $this->api->connect($this->_address, $this->_user, $this->_password);
+
+	}
+
+    public function configure(array $parsedoptions) {
+		foreach ($parsedoptions as $tobesetid => $tobesetarray ) {
+			$this->api->write ( '/' . $tobesetid . '/print' );
+			$has = $this->api->read ( true );
+			foreach ( $tobesetarray as $id => $data ) {
+				$elem = isset ( $has [$id] ) ? $has [$id] : array ();
+				$diff = array_diff ( $data, $elem );
+				$this->save_diff($diff, $elem, $tobesetid, $data);
+				if (array_key_exists ( '.id', $elem )) {
+					$save [] = $elem ['.id'];
+				}
+			}
+			foreach ( $has as $id => $value ) {
+				if (isset ( $value ['.id'] )) {
+					$collect_ids [] = $value ['.id'];
+				}
+			}
+			if (! empty ( $collect_ids ) && ! empty ( $save )) {
+				$this->remove(array_diff ( $collect_ids, $save ), $tobesetid);
+			}
+		}
+    }
+
+    protected function save_diff($diff, $elem, $tobesetid, array $data) {
+    	if (!$diff) {
+    		return;
+    	}
+		if (empty ( $elem )) {
+			$this->api->write ( '/' . $tobesetid . '/add', false );
+			$this->api->write_array ( $data );
+			$this->api->read ();
+		} else {
+			$this->api->write ( '/' . $tobesetid . '/set', false );
+			if (array_key_exists ( '.id', $elem )) {
+				$this->api->write ( '=.id=' . $elem ['.id'], false );
+			}
+			$this->api->write_array ( $diff );
+			$this->api->read ();
+		}
+    }
+
+    protected function remove($remove, $tobesetid) {
+    	if (empty($remove)) {
+    		return;
+    	}
+		$this->api->write ( '/' . $tobesetid . '/remove', false );
+		$tmp = array_shift ( $remove );
+		$command = '=.id=' . $tmp;
+		foreach ( $remove as $id => $value ) {
+			$command .= ',' . $value;
+		}
+		$this->api->write ( $command );
+		$this->api->read ();
+    }
+
+    public function disconnect() {
+  		$this->api->disconnect();
+    }
+}
+
+
+interface MtcfengineParams {
+	public function getUser();
+	public function getPassword();
+	public function getAddress();
+	public function isValid();
+	public function isHelp();
+	public function emptyParams();
+	public function getDebug();
+	public function getTimeout();
+	public function getDelay();
+	public function getConfigFile();
+}
+
+class MtcfengineCLIParams implements MtcfengineParams {
+	private $required = array('u' => '0', 'p' => '0', 'a' => '0');
+
+	public function __construct() {
+		$this->params = getopt("p:u:a:vhl:t:c:");
+	}
+
+	public function getUser() {
+		return $this->getOption('u');
+	}
+
+	public function getPassword() {
+		return $this->getOption('p');
+	}
+
+	public function getAddress() {
+		return $this->getOption('a');
+	}
+
+	public function getConfigFile() {
+    	return $this->getOption('c', $this->getAddress() . 'conf');
+    }
+
+    public function getDebug() {
+    	return $this->getOption('v');
+	}
+
+    public function isHelp() {
+		return $this->getOption('h');
+	}
+
+    public function getDelay() {
+    	return $this->getIntOption('l', $default = 2);
+    }
+    public function getTimeout() {
+    	return $this->getIntOption('t', $default = 2);;
+    }
+
+	public function emptyParams() {
+		return empty($this->params);
+	}
+
+	public function isValid() {
+		return count(array_values(array_intersect_key($this->params, $this->required))) == count($this->required);
+	}
+
+	private function getIntOption($key, $default = 0) {
+		return is_int($key, $this->params) ? $this->params[$key] : $default;
+	}
+
+	private function getOption($key, $default = false) {
+		return array_key_exists($key, $this->params) ? $this->params[$key] : $default;
+	}
+}
+
+$options = new MtcfengineCLIParams();
+
+if (!$options->isValid() || $options->isHelp() || $options->emptyParams()) {
+	show_help();
+	exit;
+}
+
+if (!file_exists($options->getConfigFile())) {
+	show_file_error($options->getConfigFile());
+	exit;
+}
+
+$parser = new Parser($options->getConfigFile());
+
+$configurator = new Mtcfengine($options->getAddress(), $options->getUser(), $options->getPassword());
+
+if (!$configurator->connect()) {
+	show_connection_error();
+	exit();
+}
+
+$configurator->configure($parser->parseFile());
+$configurator->disconnect();
+
 ?>
--- routeros_api.class.php
+++ routeros_api.class.php
@@ -1,209 +1,132 @@
 <?php
-//
-// RouterOS API class
-// Author: Denis Basta
-//
-// read() function altered by Nick Barnes to take into account the placing
-// of the "!done" reply and also correct calculation of the reply length.
-///
-// read() function altered by Ben Menking (ben@infotechsc.com); removed
-// echo statement that dumped byte data to screen
-//
-///////////////////////////
-// Revised by: Jeremy Jefferson (http://jeremyj.com)
-// January 8, 2010
-//
-//	Fixed write function in order to allow for queries to be executed
-//
 
 class routeros_api {
-
 	var $debug = false;			// Show debug information
+	var $error = true;
+	var $info = true;
 	var $error_no;				// Variable for storing connection error number, if any
 	var $error_str;				// Variable for storing connection error text, if any
-
 	var $attempts = 2;			// Connection attempt count
 	var $connected = false;		// Connection state
 	var $delay = 2;				// Delay between connection attempts in seconds
 	var $port = 8728;			// Port to connect to
 	var $timeout = 2;			// Connection attempt timeout and data read timeout
-
 	var $socket;				// Variable for storing socket resource
-
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function debug($text) {
-
 		if ($this->debug)
 			echo $text . "\n";
-
 	}
 
+	function info($text) {
+		if ($this->info)
+			echo $text . "\n";
+	}
+
+	function error($text) {
+		if ($this->error)
+			echo $text . "\n";
+	}
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function encode_length($length) {
-
 		if ($length < 0x80) {
-
 			$length = chr($length);
-
 		}
 		else
 		if ($length < 0x4000) {
-
 			$length |= 0x8000;
-
 			$length = chr( ($length >> 8) & 0xFF) . chr($length & 0xFF);
-
 		}
 		else
 		if ($length < 0x200000) {
-
 			$length |= 0xC00000;
-
 			$length = chr( ($length >> 8) & 0xFF) . chr( ($length >> 8) & 0xFF) . chr($length & 0xFF);
-
 		}
 		else
 		if ($length < 0x10000000) {
-
 			$length |= 0xE0000000;
-
 			$length = chr( ($length >> 8) & 0xFF) . chr( ($length >> 8) & 0xFF) . chr( ($length >> 8) & 0xFF) . chr($length & 0xFF);
-
 		}
 		else
 		if ($length >= 0x10000000)
 			$length = chr(0xF0) . chr( ($length >> 8) & 0xFF) . chr( ($length >> 8) & 0xFF) . chr( ($length >> 8) & 0xFF) . chr($length & 0xFF);
-
 		return $length;
-
 	}
-
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function connect($ip, $login, $password) {
-
 		for ($ATTEMPT = 1; $ATTEMPT <= $this->attempts; $ATTEMPT++) {
-
 			$this->connected = false;
-
-			$this->debug('Connection attempt #' . $ATTEMPT . ' to ' . $ip . ':' . $this->port . '...');
-
+			$this->info('Connection attempt #' . $ATTEMPT . ' to ' . $ip . ':' . $this->port . '...');
 			if ($this->socket = @fsockopen($ip, $this->port, $this->error_no, $this->error_str, $this->timeout) ) {
-
 				socket_set_timeout($this->socket, $this->timeout);
-
 				$this->write('/login');
-
 				$RESPONSE = $this->read(false);
-
 				if ($RESPONSE[0] == '!done') {
-
 					if (preg_match_all('/[^=]+/i', $RESPONSE[1], $MATCHES) ) {
-
 						if ($MATCHES[0][0] == 'ret' && strlen($MATCHES[0][1]) == 32) {
-
 							$this->write('/login', false);
 							$this->write('=name=' . $login, false);
 							$this->write('=response=00' . md5(chr(0) . $password . pack('H*', $MATCHES[0][1]) ) );
-
 							$RESPONSE = $this->read(false);
-
 							if ($RESPONSE[0] == '!done') {
-
 								$this->connected = true;
-
 								break;
-
 							}
-
 						}
-
 					}
-
 				}
-
 				fclose($this->socket);
-
 			}
-
 			sleep($this->delay);
-
 		}
-
 		if ($this->connected)
-			$this->debug('Connected...');
+			$this->info('Connected...');
 		else
-			$this->debug('Error...');
-
+			$this->error('Error...');
 		return $this->connected;
-
 	}
-
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function disconnect() {
-
 		fclose($this->socket);
-
 		$this->connected = false;
-
-		$this->debug('Disconnected...');
-
+		$this->info('Disconnected...');
 	}
-
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function parse_response($response) {
-
 		if (is_array($response) ) {
-
 			$PARSED = array();
 			$CURRENT = null;
-
 			foreach ($response as $x) {
-
 				if (in_array($x, array('!fatal', '!re', '!trap') ) ) {
-
 					if ($x == '!re')
 						$CURRENT = &$PARSED[];
 					else
 						$CURRENT = &$PARSED[$x][];
-
 				}
 				else
 				if ($x != '!done') {
-
 					if (preg_match_all('/[^=]+/i', $x, $MATCHES) )
 						$CURRENT[$MATCHES[0][0]] = (isset($MATCHES[0][1]) ? $MATCHES[0][1] : '');
-
 				}
-
 			}
-
 			return $PARSED;
-
 		}
 		else
 			return array();
-
 	}
-
 	/**************************************************
 	 *
 	 *************************************************/
-
         function array_change_key_name(&$array) {
                 if (is_array($array) ) {
                         foreach ($array as $k => $v) {
@@ -220,11 +143,9 @@ class routeros_api {
                         return $array;
                 }
         }
-
         /**************************************************
          *
          *************************************************/
-
         function parse_response4smarty($response) {
                 if (is_array($response) ) {
                         $PARSED = array();
@@ -251,125 +172,95 @@ class routeros_api {
                         return array();
                 }
         }
-
 	/**************************************************
 	 *
 	 *************************************************/
    function read($parse = true) {
-   
          $RESPONSE = array();
-	 
 	       while (true) {
-	       
-	                // Read the first byte of input which gives us some or all of the length
-			         // of the remaining reply.
-				          $BYTE = ord(fread($this->socket, 1) );
-					           $LENGTH = 0;
-						            
-						            //echo "$BYTE\n";
-							    
-							             // If the first bit is set then we need to remove the first four bits, shift left 8
-								              // and then read another byte in.
-									               // We repeat this for the second and third bits.
-										                // If the fourth bit is set, we need to remove anything left in the first byte
-												         // and then read in yet another byte.
-													          if ($BYTE & 128) {
-														              if (($BYTE & 192) == 128) {
-															                     $LENGTH = (($BYTE & 63) << 8 ) + ord(fread($this->socket, 1)) ;
-																	                 } else {
-																			                if (($BYTE & 224) == 192) {
-																					                  $LENGTH = (($BYTE & 31) << 8 ) + ord(fread($this->socket, 1)) ;
-																							                    $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																									                   } else {
-																											                     if (($BYTE & 240) == 224) {
-																													                          $LENGTH = (($BYTE & 15) << 8 ) + ord(fread($this->socket, 1)) ;
-																																                       $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																																		                            $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																																					                      } else {
-																																							                           $LENGTH = ord(fread($this->socket, 1)) ;
-																																										                        $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																																													                     $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																																															                          $LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
-																																																		                    }
-																																																				                   }
-																																																						               }
-																																																							                } else {
-																																																									            $LENGTH = $BYTE;
-																																																										             }
-																																																											     
-																																																											              // If we have got more characters to read, read them in.
-																																																												               if ($LENGTH > 0) {
-																																																													                   $_ = "";
-																																																															               $retlen=0;
-																																																																                   while ($retlen < $LENGTH) {
-																																																																		                  $toread = $LENGTH - $retlen ;
-																																																																				                 $_ .= fread($this->socket, $toread);
-																																																																						                $retlen = strlen($_);
-																																																																								            }
-																																																																									                $RESPONSE[] = $_ ;
-																																																																											            $this->debug('>>> [' . $retlen . '/' . $LENGTH . ' bytes read.');
-																																																																												             }
-																																																																													     
-																																																																													              // If we get a !done, make a note of it.
-																																																																														               if ($_ == "!done")
-																																																																															                   $receiveddone=true;
-																																																																																	   
-																																																																																	            $STATUS = socket_get_status($this->socket);
-																																																																																		    
-																																																																																		             
-																																																																																		             if ($LENGTH > 0)
-																																																																																			                 $this->debug('>>> [' . $LENGTH . ', ' . $STATUS['unread_bytes'] . '] ' . $_);
-																																																																																					 
-																																																																																					          if ( (!$this->connected && !$STATUS['unread_bytes']) ||
-																																																																																						              ($this->connected && !$STATUS['unread_bytes'] && $receiveddone) )
-																																																																																							                  break;
-																																																																																									  
-																																																																																									        }
-																																																																																										
-																																																																																										      if ($parse)
-																																																																																										               $RESPONSE = $this->parse_response($RESPONSE);
-																																																																																											       
-																																																																																											             return $RESPONSE;
-																																																																																												     
-																																																																																												        }
+			// Read the first byte of input which gives us some or all of the length
+			// of the remaining reply.
+			$BYTE = ord(fread($this->socket, 1) );
+			$LENGTH = 0;
+			//echo "$BYTE\n";
+			// If the first bit is set then we need to remove the first four bits, shift left 8
+			// and then read another byte in.
+			// We repeat this for the second and third bits.
+			// If the fourth bit is set, we need to remove anything left in the first byte
+			// and then read in yet another byte.
+			if ($BYTE & 128) {
+				if (($BYTE & 192) == 128) {
+					$LENGTH = (($BYTE & 63) << 8 ) + ord(fread($this->socket, 1)) ;
+				} else {
+				if (($BYTE & 224) == 192) {
+					$LENGTH = (($BYTE & 31) << 8 ) + ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+				} else {
+					if (($BYTE & 240) == 224) {
+					$LENGTH = (($BYTE & 15) << 8 ) + ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+						} else {
+				    $LENGTH = ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+					$LENGTH = ($LENGTH << 8 ) + ord(fread($this->socket, 1)) ;
+					}
+				}
+			}
+			} else {
+			$LENGTH = $BYTE;
+		}
+        // If we have got more characters to read, read them in.
+        if ($LENGTH > 0) {
+           $_ = "";
+           $retlen=0;
+           while ($retlen < $LENGTH) {
+           $toread = $LENGTH - $retlen ;
+           $_ .= fread($this->socket, $toread);
+           $retlen = strlen($_);
+         }
+         $RESPONSE[] = $_ ;
+         $this->debug('>>> [' . $retlen . '/' . $LENGTH . ' bytes read.');
+        }
+        // If we get a !done, make a note of it.
+         if ($_ == "!done")
+         $receiveddone=true;
+        $STATUS = socket_get_status($this->socket);
+        if ($LENGTH > 0)
+        $this->debug('>>> [' . $LENGTH . ', ' . $STATUS['unread_bytes'] . '] ' . $_);
+       if ( (!$this->connected && !$STATUS['unread_bytes']) ||
+        ($this->connected && !$STATUS['unread_bytes'] && $receiveddone) )
+         break;
+     }
+     if ($parse)
+       $RESPONSE = $this->parse_response($RESPONSE);
+      return $RESPONSE;
+     }
 	/**************************************************
 	 *
 	 *************************************************/
-
 	function write($command, $param2 = true) {
-
 		if ($command) {
-			
 			$data = explode("\n",$command);
-			
 			foreach ($data as $com) {
-				$com = trim($com);
+			$com = trim($com);
 			        fwrite($this->socket, $this->encode_length(strlen($com) ) . $com);
 			        $this->debug('<<< [' . strlen($com) . '] ' . $com);
 			}
-
-			
-
 			if (gettype($param2) == 'integer') {
 
 				fwrite($this->socket, $this->encode_length(strlen('.tag=' . $param2) ) . '.tag=' . $param2 . chr(0) );
 
 				$this->debug('<<< [' . strlen('.tag=' . $param2) . '] .tag=' . $param2);
-
 			}
 			else
 			if (gettype($param2) == 'boolean')
 				fwrite($this->socket, ($param2 ? chr(0) : '') );
-
 			return true;
-
 		}
 		else
 			return false;
-
 	}
-
 }
-
 ?>
-
