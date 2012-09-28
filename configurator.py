3c97767bfe5261be6eb44a9ba3c7c59c03ed926f
Łukasz Kostka <ukasz83@gmail.com>
2012-09-28 23:07:29 +0200

    added gnu v3 license.

--- configurator.py
+++ configurator.py
@@ -1,5 +1,17 @@
-#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import rosapi
@@ -203,7 +215,7 @@ class comparison:
 
 				#check if user specified more than one rule in menu level witch has no .id
 				if '.id' not in present_rule and present_rule and len(wanted) > 1:
-					self.log.error('more than one rule specified in no \'.id\' menu level. skipping...')
+					self.log.critical('more than one rule specified in no \'.id\' menu level. skipping...')
 					return ([], [], [])
 
 			index += 1
