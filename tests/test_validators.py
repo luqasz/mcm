4a7d0221887d83fdaad4ea43eb40f8c4cb0cc2ca
Łukasz Kostka <lukasz.kostka@netng.pl>
2014-07-08 22:48:26 +0200

    added some validators and tests

---
+++ tests/test_validators.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+try:
+    from unittest.mock import MagicMock, patch, call
+except ImportError:
+    from mock import MagicMock, patch, call
+from unittest import TestCase
+
+from validators import validate_list, validate_value, validate_type
+from exc import ValidateError
+
+
+class ValidateListTests(TestCase):
+
+    def test_validate_list_raises_ValidateError_when_list_passed_contains_forbidden_elements(self):
+        self.assertRaises( ValidateError, validate_list, lst=[1,2,3], allowed=[1,2] )
+
+
+
+class ValidateValueInTests(TestCase):
+
+    def test_validate_val_raises_ValidateError_when_value_passed_is_not_in_allowed(self):
+        self.assertRaises( ValidateError, validate_value, val='abc', allowed=('single', 'generic') )
+
+
+
+class ValidateTypeTests(TestCase):
+
+    def test_validate_type_raises_ValidateErrot_if_provided_value_is_not_the_same_type(self):
+        self.assertRaises( ValidateError, validate_type, val=1, allowed=str )
---
+++ validators.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+from exc import ValidateError
+
+
+def validate_list(lst, allowed):
+
+    forbidden = set(lst) - set(allowed)
+    if forbidden:
+        raise ValidateError()
+
+def validate_value(val, allowed):
+
+    if val not in allowed:
+        raise ValidateError()
+
+def validate_type(val, allowed):
+
+    if type(val) != allowed:
+        raise ValidateError()
