9d2af3db6230ad2375920f05fe3b948302df38a0
Łukasz Kostka <ukasz83@gmail.com>
2015-03-01 01:13:59 +0100

    Added simple main program code.

--- mcm.py
+++ mcm.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+from getpass import getpass
+
+from librouteros import LoginError, connect, ConnError
+
+from parsers import JsonParser
+from args import get_arguments
+from iodevices import ParsedFileConfig, RouterOsAPIDevice
+from adapters import SlaveAdapter, MasterAdapter
+from configurators import Configurator
+
+
+
+__version__ = 0.1
+
+
+
+def mk_slave(user, host):
+    api = connect(host=host, user=user, pw=getpass())
+    iodevice = RouterOsAPIDevice(api=api)
+    slave = SlaveAdapter(device=iodevice)
+    return slave
+
+
+def mk_master(config):
+    iodevice = ParsedFileConfig(data=config)
+    master = MasterAdapter(device=iodevice)
+    return master
+
+
+
+if __name__ == '__main__':
+    args = get_arguments(prog_version=__version__)
+    parsed_config = JsonParser(parsed=args.config)
+    master = mk_master(config=dict(parsed_config))
+
+    try:
+        slave = mk_slave(user=args.username, host=args.host)
+    except (ConnError, LoginError) as error:
+        exit(error)
+
+    configurator = Configurator(master=master, slave=slave)
+    configurator.run(paths=(path for path,rules in parsed_config))
