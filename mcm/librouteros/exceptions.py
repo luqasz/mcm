4784e3f48d3b98e9530e0ca5925a0fed65bd77a5
Łukasz Kostka <lukasz.kostka@netng.pl>
2015-10-12 23:04:22 +0200

    Added more precise exceptions.

--- mcm/librouteros/exceptions.py
+++ mcm/librouteros/exceptions.py
@@ -22,3 +22,33 @@ class ConnError( LibError ):
     '''
     Connection related errors.
     '''
+
+
+class FatalError(LibError):
+    '''
+    Exception raised when !fatal is received.
+    '''
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
+
+
+class TrapError(LibError):
+    '''
+    Exception raised when !trap is received.
+
+    :param int category: Optional integer representing category.
+    :param int tag: Optional associated .tag API attribute word.
+    :param str message: Error message.
+    '''
+
+    def __init__(self, message, category=None, tag=None):
+        self.category = category
+        self.tag = tag
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
